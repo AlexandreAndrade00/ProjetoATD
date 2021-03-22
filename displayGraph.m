36bc7d21e7c07e55cd87c3968c9aeea419795bf2
AlexandreAndrade00 <46357905+AlexandreAndrade00@users.noreply.github.com>
2021-03-22 16:46:35 +0000

    graficos com legenda e escala correta

--- displayGraph.m
+++ displayGraph.m
@@ -1,3 +1,6 @@
 function displayGraph(arrayData)
 X_axis = 1:length(arrayData);
 plot(X_axis,arrayData);
+xt = get(gca, 'XTick');
+set(gca, 'XTick', xt, 'XTickLabel', round(double(xt/50/60),2))
+xlabel('Time(min)')
