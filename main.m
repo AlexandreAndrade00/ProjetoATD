fd0078467793b0f94417f08f743dcbbf44b7c71b
AlexandreAndrade00 <46357905+AlexandreAndrade00@users.noreply.github.com>
2021-03-22 15:34:35 +0000

    graficos dos 3 eixos agora aparecem na mesma janela

--- main.m
+++ main.m
@@ -1,2 +1,11 @@
 [ACC_X, ACC_Y, ACC_Z] = openFile("InputFiles/acc_exp01_user01.txt");
+tiledlayout(3,1)
+
+nexttile
 displayGraph(ACC_X)
+
+nexttile
+displayGraph(ACC_Y)
+
+nexttile
+displayGraph(ACC_Z)
