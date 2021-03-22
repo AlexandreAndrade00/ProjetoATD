b37be49734b35c79cc0cf7b6ab2b2c26fb30a973
AlexandreAndrade00 <46357905+AlexandreAndrade00@users.noreply.github.com>
2021-03-22 15:11:45 +0000

    First commit

---
+++ displayGraph.m
@@ -0,0 +1,3 @@
+function displayGraph(arrayData)
+X_axis = 1:length(arrayData);
+plot(X_axis,arrayData);
---
+++ main.m
@@ -0,0 +1,2 @@
+[ACC_X, ACC_Y, ACC_Z] = openFile("InputFiles/acc_exp01_user01.txt");
+displayGraph(ACC_X)
