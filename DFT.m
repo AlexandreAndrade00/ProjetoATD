7ad585f1692ba33d6b177274281c76ab5ee58e08
Alexandre Andrade <alex.d.a_14@outlook.pt>
2021-05-11 16:50:29 +0100

    codigo corrigido
    usadas frequencias lineares e foi feito detrend aos dados

--- DFT.m
+++ DFT.m
@@ -1,25 +1,27 @@
-function DFT(ACC_X,ACC_Y,ACC_Z,ws,N,lower,upper)
-h = hamming(length(ACC_X(lower:upper)));
+function DFT(ACC_X,ACC_Y,ACC_Z,fs,lower,upper)
+N = upper - lower + 1;
+h = hamming(N);
 g = gausswin(length(ACC_X(lower:upper)));
 r = rectwin(length(ACC_X(lower:upper)));
 b = blackman(length(ACC_X(lower:upper)));
 
-X=fftshift(fft(ACC_X(lower:upper).*h));
-Y=fftshift(fft(ACC_Y(lower:upper).*h));
-Z=fftshift(fft(ACC_Z(lower:upper).*h));
+X=fftshift(fft(detrend(ACC_X(lower:upper)).*h));
+Y=fftshift(fft(detrend(ACC_Y(lower:upper)).*h));
+Z=fftshift(fft(detrend(ACC_Z(lower:upper)).*h));
 X(abs(X)<0.001)=0;
+Y(abs(Y)<0.001)=0;
+Z(abs(Z)<0.001)=0;
 
 if(mod(N,2)==0)
-    w=-ws/2:ws/N:ws/2-ws/N;
-    %Omega=-pi:2*pi/N:pi-2*pi/N;
+    f=-fs/2:fs/N:fs/2-fs/N;
 else
-    w=-ws/2+ws/N/2:ws/N:ws/2-ws/N/2;
-    %Omega=-pi+pi/N:2*pi/N:pi-pi/N;
+    f=-fs/2+fs/N/2:fs/N:fs/2-fs/N/2;
 end
+
 figure()
 subplot(311)
-plot(w(lower:upper),abs(X));
+plot(f,abs(X));
 subplot(312)
-plot(w(lower:upper),abs(Y));
+plot(f,abs(Y));
 subplot(313)
-plot(w(lower:upper),abs(Z));
+plot(f,abs(Z));
