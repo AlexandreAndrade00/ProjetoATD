05301be059cea987a2bbf8778ab51d8aa033bfed
Alexandre Andrade <alex.d.a_14@outlook.pt>
2021-05-14 17:28:55 +0100

    algoritmo para buscar dados sozinho

--- DFT.m
+++ DFT.m
@@ -1,16 +1,9 @@
-function DFT(ACC_X,ACC_Y,ACC_Z,fs,lower,upper)
-N = upper - lower + 1;
+function DFT(ACC_X,ACC_Y,ACC_Z,fs,lower,upper,threshold)
+N = double(upper - lower + 1);
 h = hamming(N);
 g = gausswin(length(ACC_X(lower:upper)));
 r = rectwin(length(ACC_X(lower:upper)));
 b = blackman(length(ACC_X(lower:upper)));
-
-X=fftshift(fft(detrend(ACC_X(lower:upper)).*h));
-Y=fftshift(fft(detrend(ACC_Y(lower:upper)).*h));
-Z=fftshift(fft(detrend(ACC_Z(lower:upper)).*h));
-X(abs(X)<0.001)=0;
-Y(abs(Y)<0.001)=0;
-Z(abs(Z)<0.001)=0;
 
 if(mod(N,2)==0)
     f=-fs/2:fs/N:fs/2-fs/N;
@@ -18,23 +11,20 @@ else
     f=-fs/2+fs/N/2:fs/N:fs/2-fs/N/2;
 end
 
-X(abs(X)<abs(0.8*max(X)))=0;
-Y(abs(Y)<abs(0.8*max(Y)))=0;
-Z(abs(Z)<abs(0.8*max(Z)))=0;
+data = [ACC_X ACC_Y ACC_Z];
+name = ["ACC_X" "ACC_Y" "ACC_Z"];
 
 figure()
-subplot(311)
-stem(f,abs(X));
-xlabel('Freq[Hz]')
-ylabel('Magnitude')
-title("ACC_X")
-subplot(312)
-stem(f,abs(Y));
-xlabel('Freq[Hz]')
-ylabel('Magnitude')
-title("ACC_Y")
-subplot(313)
-stem(f,abs(Z));
-xlabel('Freq[Hz]')
-ylabel('Magnitude')
-title("ACC_Z")
+i=1;
+for acc = data
+	dft=fftshift(fft(detrend(acc(lower:upper)).*h));
+	dft(abs(dft)<abs(threshold*max(dft)))=0;
+	pks = findpeaks(abs(dft));
+	amplitudes = unique((pks/N)*2)
+	subplot(310+i)
+	stem(f,abs(dft));
+	xlabel('Freq[Hz]')
+	ylabel('Magnitude')
+	title(name(i))
+	i=i+1;
+end;
