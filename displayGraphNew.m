80ba007e346597cf056a41a067900618c839fbeb
Alexandre Andrade <alex.d.a_14@outlook.pt>
2021-05-20 19:48:38 +0100

    Adicionado 2, removido figuras erradas do 3.1, e pequenas melhorias ao codigo

--- displayGraphNew.m
+++ displayGraphNew.m
@@ -1,11 +1,19 @@
-function displayGraphNew(data, labels, i, exp_atual, color)
+function displayGraphNew(data, labels, i, color, show_legend)
 fs=50;
+atividades = {"W", "W\_U", "W\_D", "SIT", "STAND", "LAY", "STAND\_SIT", "SIT\_STAND", "SIT\_LIE", "LIE\_SIT", "STAND\_LIE", "LIE\_STAND"};
 for j=1:3
-	figure(exp_atual)
+	figure(labels(i,1))
 	subplot(310+j)
 	hold on
-	N=length(data{exp_atual});
+	N=length(data{labels(i,1)});
     t = linspace(0,(N-1)/fs,N);
-	plot(t(labels(i, 4):labels(i, 5)), data{exp_atual}(labels(i, 4):labels(i, 5), j), 'Color', rgb(color))
+    if show_legend == true
+		plot(t(labels(i, 4):labels(i, 5)), data{labels(i,1)}(labels(i, 4):labels(i, 5), j), 'Color', rgb(color), 'DisplayName', atividades{labels(i,3)})
+	else
+		plot(t(labels(i, 4):labels(i, 5)), data{labels(i,1)}(labels(i, 4):labels(i, 5), j), 'Color', rgb(color))
+	end
 	hold off
+end
+if show_legend == true
+	legend
 end
